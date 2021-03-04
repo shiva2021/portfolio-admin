6d2d48c9dc380a8ab48a5533362209736f890ab3
Shivang Chaturvedi <shiva.chaturvedi2191@outlook.com>
2021-03-04 23:53:23 +0530

    initial Setup

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,15 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
-
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/login',
     name: 'login',
-    component: import(/* webpackChunkName: "group-foo" */ '../views/login.vue')
+    component: function () {
+      return import(/* webpackChunkName: "about" */ '../views/login.vue')
+    }
   },
   {
     path: '/',
---
+++ src/views/login.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="container">
+		<h1 class="is-size-2">This is the login page</h1>
+	</div>
+</template>
+
+<script>
+export default {
+	created() {
+		debugger;
+	},
+};
+</script>
+
+<style></style>
