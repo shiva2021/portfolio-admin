ef7c08b8f90aa68a782a659a11d5fbbc1ba0900e
Shivang Chaturvedi <shiva.chaturvedi2191@outlook.com>
2021-03-08 18:11:05 +0530

    Sidenav CSS

---
+++ src/components/sidebar.vue
@@ -0,0 +1,85 @@
+<template>
+	<div>
+		<!-- Sidebar -->
+		<!-- <div id="idSidebar" class="sidebar"> -->
+		<ul class="sidebar--parent">
+			<li v-for="(item, i) in sidenav" :key="i" :data-target="item.target" class="has-children is-flex">
+				<a href="javascript:void(0)">
+					<i :class="`${item.icon} is-size-6 mr-3 font-color`"></i>
+					<span class="is-size-6">{{ item.name }}</span>
+				</a>
+				<i v-if="item.hasChevron" class="fas fa-chevron-left left-icon font-color"></i>
+				<ul :id="item.target" v-if="item.children.length > 0">
+					<li v-for="(child, i) in item.children" :key="i">
+						<a href="javascript:void(0)">
+							<i :class="`${child.icon} is-size-6 mr-3 font-color`"></i>
+							<span class="is-size-6">{{ child.name }}</span>
+						</a>
+					</li>
+				</ul>
+			</li>
+		</ul>
+	</div>
+	<!-- </div> -->
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+export default {
+	computed: {
+		...mapGetters(["user", "isAuthenticated", "currentApp"]),
+	},
+
+	data() {
+		return {
+			sidenav: [
+				{
+					name: "Dashboard",
+					target: "",
+					children: [],
+					icon: "fas fa-chart-line",
+					hasChevron: false,
+				},
+				{
+					name: "Manage Content",
+					target: "content",
+					children: [
+						{
+							name: "Add Content",
+							children: [],
+							icon: "fas fa-plus",
+							hasChevron: false,
+						},
+					],
+					icon: "fas fa-heading",
+					hasChevron: true,
+				},
+			],
+		};
+	},
+	mounted() {
+		let $sidebarDropdowns = document.querySelectorAll(".has-children");
+		if ($sidebarDropdowns.length > 0) {
+			$sidebarDropdowns.forEach((el) => {
+				el.addEventListener("click", () => {
+					const target = el.dataset.target;
+					const $target = document.getElementById(target);
+
+					if ($target) {
+						$target.classList.toggle("has-list-active");
+						//Rotate Icon
+						let icon = el.getElementsByClassName("left-icon");
+						icon[0].classList.toggle("rotate-icon");
+					}
+				});
+			});
+		}
+	},
+};
+</script>
+
+<style lang="scss" scoped>
+hr {
+	background-color: #3d4957;
+}
+</style>
--- src/views/portfolio/dashboard.vue
+++ src/views/portfolio/dashboard.vue
@@ -1,92 +1,16 @@
 <template>
-	<div>
-		<!-- Sidebar -->
-		<div id="idSidebar" class="sidebar">
-			<ul class="sidebar--parent">
-				<li>
-					<a href="javascript:void(0)">
-						<i class="fas fa-chart-line is-size-6 mr-3 font-color"></i>
-						<span class="is-size-6">Item1</span>
-						<i class="fas fa-chevron-left left-icon font-color"></i>
-					</a>
-				</li>
-				<li>
-					<a href="javascript:void(0)">
-						<i class="fas fa-chart-line is-size-6 mr-3 font-color"></i>
-						<span class="is-size-6">Item1</span>
-						<i class="fas fa-chevron-left left-icon font-color"></i>
-					</a>
-				</li>
-				<li>
-					<a href="javascript:void(0)">
-						<i class="fas fa-chart-line is-size-6 mr-3 font-color"></i>
-						<span class="is-size-6">Item1</span>
-						<i class="fas fa-chevron-left left-icon font-color"></i>
-					</a>
-				</li>
-				<li data-target="list1" class="has-children">
-					<a href="javascript:void(0)">
-						<i class="fas fa-chart-line is-size-6 mr-3 font-color"></i>
-						<span class="is-size-6">Item1</span>
-						<i class="fas fa-chevron-left left-icon font-color"></i>
-					</a>
-					<ul id="list1">
-						<li>
-							<a href="javascript:void(0)">
-								<i class="fas fa-chart-line is-size-6 mr-3 font-color"></i>
-								<span class="is-size-6">Sub-Item1</span>
-							</a>
-						</li>
-					</ul>
-				</li>
-
-				<li data-target="list2" class="has-children">
-					<a href="javascript:void(0)">
-						<i class="fas fa-chart-line is-size-6 mr-3 font-color"></i>
-						<span class="is-size-6">Item1</span>
-						<i class="fas fa-chevron-left left-icon font-color"></i>
-					</a>
-					<ul id="list2">
-						<li>
-							<a href="javascript:void(0)">
-								<i class="fas fa-chart-line is-size-6 mr-3 font-color"></i>
-								<span class="is-size-6">Sub-Item1</span>
-							</a>
-						</li>
-					</ul>
-				</li>
-			</ul>
-		</div>
+	<div class="dashboard">
+		<Sidebar></Sidebar>
 	</div>
 </template>
 
 <script>
-import { mapGetters } from "vuex";
+import Sidebar from "@/components/sidebar.vue";
 export default {
-	computed: {
-		...mapGetters(["user", "isAuthenticated", "currentApp"]),
-	},
-	mounted() {
-		let $sidebarDropdowns = document.querySelectorAll(".has-children");
-		if ($sidebarDropdowns.length > 0) {
-			$sidebarDropdowns.forEach((el) => {
-				el.addEventListener("click", () => {
-					const target = el.dataset.target;
-					const $target = document.getElementById(target);
-
-					$target.classList.toggle("has-list-active");
-					//Rotate Icon
-					let icon = el.getElementsByClassName("left-icon");
-					icon[0].classList.toggle("rotate-icon");
-				});
-			});
-		}
+	components: {
+		Sidebar,
 	},
 };
 </script>
 
-<style lang="scss" scoped>
-hr {
-	background-color: #3d4957;
-}
-</style>
+<style></style>
