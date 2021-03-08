012f780d7aa96c4c21b49ba0f5567a8c0abd54dc
Shivang Chaturvedi <shiva.chaturvedi2191@outlook.com>
2021-03-08 15:28:17 +0530

    Logic for expanding and collapsing sidemenu bar

--- src/views/portfolio/dashboard.vue
+++ src/views/portfolio/dashboard.vue
@@ -4,35 +4,51 @@
 		<div id="idSidebar" class="sidebar">
 			<ul class="sidebar--parent">
 				<li>
-					<a href="">
+					<a href="javascript:void(0)">
 						<i class="fas fa-chart-line is-size-6 mr-3 font-color"></i>
 						<span class="is-size-6">Item1</span>
 						<i class="fas fa-chevron-left left-icon font-color"></i>
 					</a>
 				</li>
 				<li>
-					<a href="">
+					<a href="javascript:void(0)">
 						<i class="fas fa-chart-line is-size-6 mr-3 font-color"></i>
 						<span class="is-size-6">Item1</span>
 						<i class="fas fa-chevron-left left-icon font-color"></i>
 					</a>
 				</li>
 				<li>
-					<a href="">
+					<a href="javascript:void(0)">
 						<i class="fas fa-chart-line is-size-6 mr-3 font-color"></i>
 						<span class="is-size-6">Item1</span>
 						<i class="fas fa-chevron-left left-icon font-color"></i>
 					</a>
 				</li>
-				<li>
-					<a href="">
+				<li data-target="list1" class="has-children">
+					<a href="javascript:void(0)">
 						<i class="fas fa-chart-line is-size-6 mr-3 font-color"></i>
 						<span class="is-size-6">Item1</span>
 						<i class="fas fa-chevron-left left-icon font-color"></i>
 					</a>
-					<ul>
+					<ul id="list1">
 						<li>
-							<a href="">
+							<a href="javascript:void(0)">
+								<i class="fas fa-chart-line is-size-6 mr-3 font-color"></i>
+								<span class="is-size-6">Sub-Item1</span>
+							</a>
+						</li>
+					</ul>
+				</li>
+
+				<li data-target="list2" class="has-children">
+					<a href="javascript:void(0)">
+						<i class="fas fa-chart-line is-size-6 mr-3 font-color"></i>
+						<span class="is-size-6">Item1</span>
+						<i class="fas fa-chevron-left left-icon font-color"></i>
+					</a>
+					<ul id="list2">
+						<li>
+							<a href="javascript:void(0)">
 								<i class="fas fa-chart-line is-size-6 mr-3 font-color"></i>
 								<span class="is-size-6">Sub-Item1</span>
 							</a>
@@ -50,11 +66,41 @@ export default {
 	computed: {
 		...mapGetters(["user", "isAuthenticated", "currentApp"]),
 	},
+	mounted() {
+		let $sidebarDropdowns = document.querySelectorAll(".has-children");
+		if ($sidebarDropdowns.length > 0) {
+			$sidebarDropdowns.forEach((el) => {
+				el.addEventListener("click", () => {
+					const target = el.dataset.target;
+					const $target = document.getElementById(target);
+
+					$target.classList.toggle("has-list-active");
+					//Rotate Icon
+					let icon = el.getElementsByClassName("left-icon");
+					icon[0].classList.toggle("rotate-icon");
+				});
+			});
+		}
+	},
 };
 </script>
 
 <style lang="scss" scoped>
 hr {
 	background-color: #3d4957;
+}
+
+.has-children ul {
+	display: none;
+	transition: display 3s;
+}
+
+.rotate-icon {
+	transition: 0.5s;
+	transform: rotate(-90deg);
+}
+
+.has-list-active {
+	display: block !important;
 }
 </style>
